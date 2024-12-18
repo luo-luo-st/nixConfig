e1b6ab77cc61d40549e5d5010e1e8045109341f5
luo-luo-st <488753681@qq.com>
2024-12-18 14:32:53 +0800

    debug

--- home/gnome.nix
+++ home/gnome.nix
@@ -5,32 +5,18 @@ let
 in
 {
   imports = [
-    # ../../../pkgs/wallpaper-hm
+    
   ];
-  home.file.".face".source = avatar; #头像
+  home.file.".face".source = avatar; 
   dconf = {
     enable = true;
     settings = {
       "org/gnome/shell" = {
         disable-user-extensions = false;
         enabled-extensions = [
-          "appindicatorsupport@rgcjonas.gmail.com" #系统托盘
-          #"dash-to-dock@micxgx.gmail.com"
-          "clipboard-history@alexsaveau.dev" # 剪切板
-          #"kimpanel@kde.org"
-          "apps-menu@gnome-shell-extensions.gcampax.github.com"
-          #"auto-move-windows@gnome-shell-extensions.gcampax.github.corm"
-          #"drive-menu@gnome-shell-extensions.gcampax.github.com"
-          #"launch-new-instance@gnome-shell-extensions.gcampax.github.com"
-          #"light-style@gnome-shell-extensions.gcampax.github.com"
-          #"native-window-placement@gnome-shell-extensions.gcampax.github.com"
-          #"places-menu@gnome-shell-extensions.gcampax.github.com"
+          "appindicatorsupport@rgcjonas.gmail.com" 
           "window-list@gnome-shell-extensions.gcampax.github.com"
           "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
-          #"workspace-indicator@gnome-shell-extensions.gcampax.github.com"
-
-          #"paperwm@paperwm.github.com"
-          #"material-shell@papyelgringo"
         ];
         favorite-apps = [
           "firefox.desktop"
@@ -40,21 +26,21 @@ in
 
       };
       "org/gnome/desktop/background" = {
-        # 壁纸
+        
         picture-uri = wallpaper;
         picture-uri-dark = wallpaper;
       };
       "org/gnome/mutter" = {
-        # 新窗口在屏幕中心打开
+        
         center-new-windows = true;
       };
 
       "org/gnome/desktop/interface" = {
-        # gtk-theme="Adwaita-dark";
+        
         clock-show-date = true;
         clock-show-weekday = true;
       };
-      #电源和做锁屏 ===============
+      
       "org/gnome/desktop/screensaver" = {
         lock-delay = 0;
         lock-enabled = true;
@@ -62,8 +48,8 @@ in
       "org/gnome/desktop/session" = {
         idle-delay = -1;
       };
-      #gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
-      #gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+      
+      
       "org.gnome.settings-daemon.plugins.power" = {
         sleep-inactive-ac-type = "nothing";
         sleep-inactive-battery-type = "nothing";
@@ -75,32 +61,18 @@ in
         sleep-inactive-battery-timeout = 0;
         sleep-inactive-battery-type = "nothing";
       };
-      #电源和做锁屏 ===============
+      
 
       "org/gnome/desktop/wm/preferences" = {
-        resize-with-right-button = true; #win+右键 修改窗口大小  
-        button-layout = "appmenu:minimize,close"; #去掉最大化按钮 没啥卵用
+        resize-with-right-button = true; 
+        button-layout = "appmenu:minimize,close"; 
         focus-mode = "sloppy";
       };
 
       "org/gnome/desktop/wm/keybindings" = {
-        close = [ "<Alt>F4" "<Super><Shift>q" ]; # 关闭窗口
-        show-desktop = [ "<Super>d" ]; # win+d 显示桌面壁纸
-      };
-      "org/gnome/settings-daemon/plugins/media-keys" = {
-        custom-keybindings = [
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
-        ];
-        home = [ "<Super>e" ]; # 打开文件home 使用默认文件管理器
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Super>Return";
-        command = "nu -e 'echo $env.PWD'";
-        name = "Open nushell";
+        close = [ "<Alt>F4" "<Super><Shift>q" ]; 
+        show-desktop = [ "<Super>d" ]; 
+        home = [ "<Super>e" ]; 
       };
     };
   };
--- modules/sysgnome.nix
+++ modules/sysgnome.nix
@@ -1,9 +1,5 @@
 {pkgs, ...} : {
-  services.xserver = {
-  enable = true;
-  displayManager.gdm.enable = true;
-  desktopManager.gnome.enable = true;
-};
+  services.xserver.displayManager.gdm.wayland = true;
 environment.gnome.excludePackages = (with pkgs; [
   gnome-photos
 ]) ++ (with pkgs; [
