3d74614196c752a84fe44ac817d83f1b16e6592a
luo-luo-st <488753681@qq.com>
2024-12-23 23:14:42 +0800

    debug

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -6,6 +6,7 @@
     [
       ../../modules/system.nix
       ../../modules/sysgnome.nix
+      ../../modules/sql/postgreSQL.nix
       ../../pkgs/variables.nix
       ../../pkgs/kanata.nix
       ../../pkgs/i18ninput
