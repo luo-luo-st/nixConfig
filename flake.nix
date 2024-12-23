184e74a245ef152af87db630bbb2f76b780afa4d
luo-luo-st <488753681@qq.com>
2024-12-24 01:26:15 +0800

    debug

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       nixos = let
         username = "luoluo";
         system = "x86_64-linux";
-        specialArgs = {inherit username system inputs;};
+        specialArgs = {inherit username system;};
       in
         nixpkgs.lib.nixosSystem {
           inherit specialArgs;
